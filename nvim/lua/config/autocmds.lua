00bcf8e0a92009ada4c2f2b0a6db6a3c679b356d
yuhua.zhao <yuhua.zhao@happyelements.com>
2024-01-05 18:43:36 +0800

    add code

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,6 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
--- 
+--
 
-vim.o.clipboard="unnamed"
+vim.o.clipboard = "unnamed"
+
+-- Disable autoformat for lua files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "lua", "python" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,22 +1,19 @@
 return {
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   -- opts = {
+  --   --   servers = {
+  --   --     pyright = {},
+  --   --     tsserver = {},
+  --   --   },
+  --   -- },
+  -- },
   {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
-        tsserver = {},
-      },
-    },
-    init = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-
-      local format = function()
-        require("lazyvim.plugins.lsp.format").format({ force = true, timeout_ms = 3000 })
-      end
-
-      keys[#keys + 1] = { "<leader>cf", format, desc = "Format Document ---", has = "formatting" }
-      keys[#keys + 1] = { "<leader>cf", format, desc = "Format Range ---", mode = "v", has = "rangeFormatting" }
+    "stevearc/conform.nvim",
+    lazy = false,
+    opts = function(_, opts)
+      opts.formatters_by_ft.python = { "black" }
+      return opts
     end,
   },
 }
