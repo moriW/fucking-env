6d3be6e014fdec7877e3740eba72c16b0526cb13
mori <moridisa@moridisa.com>
2024-01-06 16:15:40 +0800

    update

--- init.sh
+++ init.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
+git submodule update
+
 # install brew
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install tools
-# brew install k9s nvim
+brew install k9s nvim tmux alacritty thefuck autojump
+
+# font
+brew tap homebrew/cask-fonts
+brew install font-fira-code-nerd-font
+
+# oh-my-zsh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 
 rm -rf ~/.config/nvim
-rm ~/.alacritty.yml
+rm ~/.alacritty.toml
+rm ~/.zshrc
 rm ~/.tmux.conf
 rm ~/.tmux.conf.local
 
